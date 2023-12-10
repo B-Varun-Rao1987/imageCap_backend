bdadd5ac11e50c7cf60acf64b41e1d6ea7f2ca59
B-Varun-Rao1987 <itsvarunrao888@gmail.com>
2023-12-11 04:40:09 +0530

    commit5

--- middleware/fetchUser.js
+++ middleware/fetchUser.js
@@ -1,16 +1,15 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = "bhushanlinalmanasiniket";
 
 const fetchUser = (req, res, next) => {
     // Get the user from jwt token and add id to request object
     const token = req.header('token');
 
     if (!token) {
-        res.status(401).send({ error: "ePlease authenticate using a valid token" });
+        res.status(401).send({ error: "Please authenticate using a valid token" });
     }
 
     try {
-        const data = jwt.verify(token, JWT_SECRET);         // check what it returns
+        const data = jwt.verify(token, process.env.JWT_SECRET);         // check what it returns
         // console.log('middleware ',data);
         req.user = data;     // 'req.user' is a object of user data, going to use everywhere where we are using middleware(fetchUser)
         next();
